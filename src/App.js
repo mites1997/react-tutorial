dff28044bc99f01e8817480136d71d4e45f13c10
mites1997 <arnabfeb15@gmail.com>
2022-01-08 18:44:02 +0530

    add a code change to accomodate react props for data passing across components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2022, 1, 8) },
+    { title: "Car Insurance2", amount: 294.67, date: new Date(2022, 1, 8) },
+    { title: "Car Insurance3", amount: 294.67, date: new Date(2022, 1, 8) },
+    { title: "Car Insurance4", amount: 294.67, date: new Date(2022, 1, 8) },
+  ];
   return (
     <div>
       <h2>Let's Get Started!!!!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+       <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
