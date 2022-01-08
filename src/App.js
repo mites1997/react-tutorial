245ca8f9233ea465c37276bcf65f8766da16b5c9
mites1997 <arnabfeb15@gmail.com>
2022-01-08 18:53:19 +0530

    add logic to split date to a readable format

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2022, 1, 8) },
-    { title: "Car Insurance2", amount: 294.67, date: new Date(2022, 1, 8) },
-    { title: "Car Insurance3", amount: 294.67, date: new Date(2022, 1, 8) },
-    { title: "Car Insurance4", amount: 294.67, date: new Date(2022, 1, 8) },
+    { title: "Book", amount: 29.67, date: new Date(2022, 1, 8) },
+    { title: "Table", amount: 94.67, date: new Date(2022, 1, 8) },
+    { title: "Chair", amount: 24.67, date: new Date(2022, 1, 8) },
   ];
   return (
     <div>
