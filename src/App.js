f38fdd55167ebf297d78e408f3a3dd03f1901ecf
mites1997 <arnabfeb15@gmail.com>
2022-01-09 12:50:15 +0530

    reorder folder structure in the project to a much more readable format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2022, 1, 8) },
